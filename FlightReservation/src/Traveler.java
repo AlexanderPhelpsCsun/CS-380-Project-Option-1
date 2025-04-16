b37f99b61d3e24610bd0faa258b97cd35a298080
valeriemay00 <vchavez8500@gmail.com>
2025-04-16 16:44:25 -0700

    updated flight and traveler clasess

--- FlightReservation/src/Traveler.java
+++ FlightReservation/src/Traveler.java
@@ -1,7 +1,13 @@
 
 
-
-
 public class Traveler {
+    int id;
+    String name;
+    String email;
+
     
+}
+public Traveler (String name, String email){
+    this.name = name;
+    this.email = email;
 }

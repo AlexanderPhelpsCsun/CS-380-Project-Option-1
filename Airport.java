163c8ee77c8dc4612aa1d542626c26754e93074e
AlexanderPhelpsCsun <alexander.phelps.609@my.csun.edu>
2025-04-17 13:10:36 -0700

    Updated Airport.java with documentation

--- Airport.java
+++ Airport.java
@@ -1,17 +1,32 @@
 import java.util.ArrayList;
 
+/**
+Airport class represents the airports that you can book flights too and from. Airport objects will keep a List of aircrafts that are present at the airport.
+@Author: Alexander
+@Version: 1.0
+*/
 public class Airport {
     String location;
-    ArrayList<aircraft> presentAircrafts = new ArrayList<>();
+    ArrayList<Aircraft> presentAircrafts = new ArrayList<>();
 
-
-
-    void addFlight(Flight flight)
+/**
+Adds an aircraft to the present aircrafts list
+@param: Aircraft object
+@return: void
+*/
+    void addAircraft(Aircraft aircraft)
     {
-        presentAircrafts.add(flight);
+        presentAircrafts.add(aircraft);
     }
-    void removeFlight(Flight flight)
+//*
+Removes an aircraft from the present aircrafts list
+@param: Aircraft object
+@return: void
+*/
+
+    void removeAircraft(Aircraft aircraft)
     {
-        presentAircrafts.remove(flight);
+        presentAircrafts.remove(aircraft);
     }
 }
+

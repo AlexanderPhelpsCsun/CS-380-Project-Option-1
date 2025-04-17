4b1a9a0122617de920f6f40cffe799cddf2140e0
AlexanderPhelpsCsun <alexander.phelps.609@my.csun.edu>
2025-04-17 14:18:05 -0700

    Update CreditPayment.java Documentation

--- Payment/CreditPayment.java
+++ Payment/CreditPayment.java
@@ -1,4 +1,12 @@
+
+
+
 import java.util.Scanner;
+/**
+Handles credits payments. CreditPayment class which will inherits from Payment class. 
+@author Alexander
+@version 0.8
+*/
 
 public class CreditPayment extends Payment {
 
@@ -10,10 +18,20 @@ public class CreditPayment extends Payment {
     String pin;
     String expiration;
    
+
+/**
+Constructor that initliazes amount that must be payed and the isPaid flag is set to false
+@param Amount of money the transaction requires to complete passed as a Double
+*/
+
     public CreditPayment(double amount) {
         super(amount);
     }
-
+/**
+InputCredit will take the credit info and process it. It goes through System.in and System.out.
+@param none
+@return void
+*/
     public void inputCredit()
     {
         System.out.print("Enter Credit card Number: ");
@@ -25,6 +43,12 @@ public class CreditPayment extends Payment {
         //if(creditCardNumber.leng)
     }
 
+
+/**
+	The actual payment function which sets the Ispaid flag to true after the payment
+	@param Cost of transaction in double format
+	@return void
+    */
     public void pay(double amount)
     {
         if(hasPaymentInfo == true)
@@ -38,11 +62,13 @@ public class CreditPayment extends Payment {
     }
 
 
-
+/**
+Used to check payment status for other classes/methods to determine if the user can be issued a ticket/confirmation yet
+@return true if payment is complete, return false if payment isn’t complete.
+*/
     public boolean getPaymentStatus()
     {
         return isPaid;
     }
 }
-
 

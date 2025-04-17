3a625ebdfaf7f8d5387d2175d3e55cae7823ca73
AlexanderPhelpsCsun <alexander.phelps.609@my.csun.edu>
2025-04-17 14:29:45 -0700

    Update CashPayment.java

--- Payment/CashPayment.java
+++ Payment/CashPayment.java
@@ -1,26 +1,29 @@
 /**
-CashPayment class is a child of abstract payment class and handles basic cash payments for flight reservations.
-@Author Alexander
-@Version 1.0
+*CashPayment class is a child of abstract payment class and handles basic cash payments for flight reservations.
+*
+*@author Alexander
+*@version 1.0
 */
 
 public class CashPayment extends Payment 
 {
     boolean isPaid = false;
     /**
-	Constructor which initializes amount to be paid
-	@param Cost of transaction in double format
+    *Constructor which initializes amount to be paid
+    
+    *@param Cost of transaction in double format
     */
     public CashPayment(double amount) {
         super(amount);
     }
 
 
-/**
-	The actual payment function which sets the Ispaid flag to true after the payment
-	@param Cost of transaction in double format
-	@return void
-    */
+   /**
+   *The actual payment function which sets the Ispaid flag to true after the payment
+	
+   *@param Cost of transaction in double format
+   *@return void
+   */
     public void pay(double amount)
     {
         isPaid = true;
@@ -28,8 +31,9 @@ public class CashPayment extends Payment
 
 
 /**
-Used to check payment status for other classes/methods to determine if the user can be issued a ticket/confirmation yet
-@return true if payment is complete, return false if payment isn’t complete.
+*Used to check payment status for other classes/methods to determine if the user can be issued a ticket/confirmation yet
+*
+*@return true if payment is complete, return false if payment isn’t complete.
 */
     public boolean getPaymentStatus()
     {

6973f6b79fdd6198a122c5321c82a151bfe0b746
AlexanderPhelpsCsun <alexander.phelps.609@my.csun.edu>
2025-04-17 16:46:59 -0700

    Aircraft Class

---
+++ Aircraft.java
@@ -0,0 +1,18 @@
+/**
+simple aircraft class that just stores an aircraft id so the airports and flights can identify them
+@author Alexander
+@version 1.0
+*/
+public class Aircraft {
+    String id;
+
+/**
+*Constructor makes an aircraft and assings it an id
+*
+*@param String id
+*/
+    public Aircraft(String id)
+    {
+        id = id;
+    }
+}

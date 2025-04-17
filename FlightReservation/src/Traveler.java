1ad87b85b5765f0e6113d08dba21936e05e169fc
valeriemay00 <vchavez8500@gmail.com>
2025-04-17 16:17:20 -0700

    source code documentation added

--- FlightReservation/src/Traveler.java
+++ FlightReservation/src/Traveler.java
@@ -19,14 +19,17 @@
  * - None; this class is purely for data modeling.
  */
 
+ * @author Valerie Chavez
+ */
 public class Traveler {
-    //  Properties 
+    // === Properties ===
     private int travelerID;
     private String name;
     private String email;
     private String phone;
 
-    // Constructors 
+    // === Constructors ===
+
     /**
      * Default constructor.
      */
@@ -49,34 +52,74 @@ public class Traveler {
 
     // === Getter and Setter Methods ===
 
+    /**
+     * Gets the traveler's ID.
+     *
+     * @return the traveler's ID
+     */
     public int getTravelerID() {
         return travelerID;
     }
 
+    /**
+     * Sets the traveler's ID.
+     *
+     * @param travelerID the traveler's ID to set
+     */
     public void setTravelerID(int travelerID) {
         this.travelerID = travelerID;
     }
 
+    /**
+     * Gets the traveler's name.
+     *
+     * @return the traveler's name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the traveler's name.
+     *
+     * @param name the traveler's name to set
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets the traveler's email.
+     *
+     * @return the traveler's email
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Sets the traveler's email.
+     *
+     * @param email the traveler's email to set
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Gets the traveler's phone number.
+     *
+     * @return the traveler's phone number
+     */
     public String getPhone() {
         return phone;
     }
 
+    /**
+     * Sets the traveler's phone number.
+     *
+     * @param phone the traveler's phone number to set
+     */
     public void setPhone(String phone) {
         this.phone = phone;
     }

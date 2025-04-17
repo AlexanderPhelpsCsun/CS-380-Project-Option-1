d94e71b9c784b25845dd134dda6cc8aaf28d5ee1
ITZ262ME <112421768+ITZ262ME@users.noreply.github.com>
2025-04-17 16:20:07 -0700

    Add files via upload
    Created A Verma Air Springboot toolkit project

---
+++ vermaAir/src/main/java/group4/project/vermaAir/VermaAirApplication.java
@@ -0,0 +1,13 @@
+package group4.project.vermaAir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VermaAirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VermaAirApplication.class, args);
+	}
+
+}
---
+++ vermaAir/src/test/java/group4/project/vermaAir/VermaAirApplicationTests.java
@@ -0,0 +1,13 @@
+package group4.project.vermaAir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VermaAirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

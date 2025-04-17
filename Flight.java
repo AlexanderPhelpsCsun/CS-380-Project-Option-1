90b66cf87fc4fa409a045da2d974c4df991e3db1
AlexanderPhelpsCsun <alexander.phelps.609@my.csun.edu>
2025-04-17 16:45:57 -0700

    Update Flight.java

--- Flight.java
+++ Flight.java
@@ -17,58 +17,57 @@ public class Flight {
     /**
     *The flight constructor makes the flight object and sets origin, destination, price and whether or not it is full
     *
-    *@param String origin where the flight is departing from
-    *@param String destination where the flight is landing
-    *@param double price is the price for one ticket on the flight
+    *@param origin where the flight is departing from
+    *@param destination where the flight is landing
+    *@param price is the price for one ticket on the flight
     *@param isFull boolean that returns whether or not the flight has availability
-    *return Flight object
     */
     
     public Flight(String origin, String destination, double price, boolean isFull)
     {
-        origin = origin;
-        destination = destination;
-        price = price;
-        isFull = isFull;
+        this.origin = origin;
+        this.destination = destination;
+        this.price = price;
+        this.isFull = isFull;
     }
 
     /**
     *getAvailability returns a boolean with whether or not the flight is full
     *
-    *@return boolean true if flight has vacancy returns false if flight is full
+    *@return true if flight has vacancy returns false if flight is full
     */
-    boolean getAvailability()
+    public boolean getAvailability()
     {
-        return !isFull;
+        return !this.isFull;
     }
     /**
     *getPrice is to check the price of a flight
     *
-    *@return double with the price
+    *@return the price
     */
-    double getPrice()
+    public double getPrice()
     {
-        return price;
+        return this.price;
     } 
 
     /**
     *getDestination is to access where the flight is going to
     *
-    *@return String of Destination
+    *@return Destination
     */
-    String getDestination()
+    public String getDestination()
     {
-        return destination;
+        return this.destination;
     }
 
     /**
     *getOrigin is to get where the flight is departing from
     *
-    *@return String of origin
+    *@return origin
     */
-    String getOrigin()
+    public String getOrigin()
     {
-        return origin;
+        return this.origin;
     }
 
 }

f6588c5e8aaa704d2af4105513526c3e09140b74
AlexanderPhelpsCsun <alexander.phelps.609@my.csun.edu>
2025-04-17 14:41:01 -0700

    Update Payment.java

--- Payment/Payment.java
+++ Payment/Payment.java
@@ -1,7 +1,8 @@
 /**
-Abstract payment class which will be the parent for CashPayment class and for CreditPayment class
-@author Alexander
-@version 1.0
+*Abstract payment class which will be the parent for CashPayment class and for CreditPayment class
+
+*@author Alexander
+*@version 1.0
 */
 
 abstract class Payment {
@@ -9,8 +10,9 @@ abstract class Payment {
     boolean isPaid;
 
 /**
-Constructor that initliazes amount that must be payed and the isPaid flag is set to false
-@param Amount of money the transaction requires to complete passed as a Double
+*Constructor that initliazes amount that must be payed and the isPaid flag is set to false
+
+*@param Amount of money the transaction requires to complete passed as a Double
 */
     public Payment(double amount) {
         this.amount = amount;
@@ -18,15 +20,17 @@ Constructor that initliazes amount that must be payed and the isPaid flag is set
     }
 
 /**
-	The actual payment function which sets the Ispaid flag to true after the payment
-	@param Cost of transaction in double format
-	@return void
-    */
+*The actual payment function which sets the Ispaid flag to true after the payment
+
+*@param Cost of transaction in double format
+*@return void
+*/
 
     public abstract void pay(double amount);
 /**
-Used to check payment status for other classes/methods to determine if the user can be issued a ticket/confirmation yet
-@return true if payment is complete, return false if payment isn’t complete.
+*Used to check payment status for other classes/methods to determine if the user can be issued a ticket/confirmation yet
+
+*@return true if payment is complete, return false if payment isn’t complete.
 */
 
 

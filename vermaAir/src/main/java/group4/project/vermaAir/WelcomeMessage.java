19df1f720a4d2108a788c4312aa21800ea1bc2f7
ITZ262ME <112421768+ITZ262ME@users.noreply.github.com>
2025-04-28 01:47:31 -0700

    Add files via upload

---
+++ vermaAir/src/main/java/group4/project/vermaAir/WelcomeMessage.java
@@ -0,0 +1,10 @@
+package group4.project.vermaAir;
+
+import org.springframework.stereotype.Component;
+@Component 
+public class WelcomeMessage {
+	
+		public String getWelcomeMessage() {
+			return "Welcome to Spring Boot Application";
+		}
+}
---
+++ vermaAir/src/main/java/group4/project/vermaAir/controller.java
@@ -0,0 +1,13 @@
+package group4.project.vermaAir;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class controller {
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/welcome.html";
+    }
+}

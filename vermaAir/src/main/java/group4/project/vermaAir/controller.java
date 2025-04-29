7f3082f171c4fbe0781c51d75130bad91a17418f
ITZ262ME <112421768+ITZ262ME@users.noreply.github.com>
2025-04-29 10:28:14 -0700

    Update controller.java

--- vermaAir/src/main/java/group4/project/vermaAir/controller.java
+++ vermaAir/src/main/java/group4/project/vermaAir/controller.java
@@ -6,8 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class controller {
 
-    @GetMapping("/")
-    public String home() {
-        return "redirect:/welcome.html";
-    }
+	@GetMapping("/")
+	public String home() {
+	    return "welcome";
+	}S
+
 }

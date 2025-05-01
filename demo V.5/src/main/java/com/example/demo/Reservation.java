838b503b2bffc10681a051da80748da59ba298fe
AlexanderPhelpsCsun <alexander.phelps.609@my.csun.edu>
2025-05-01 12:29:36 -0700

    Delete demo V.5 directory

--- demo V.5/src/main/java/com/example/demo/Reservation.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-public class Reservation {
-    private int flightID;
-    private String origin;
-    private String destination;
-    private String departureTime;
-    
-}
